0a69d524b3fc695411b03503195b2ca6d7e18ced
Wandasiewicz <148969665+Wandasiewicz@users.noreply.github.com>
2024-01-23 21:52:15 +0100

    tresc blok 2

---
+++ tresc.cpp
@@ -0,0 +1,13 @@
+B PODSTAWY PROGRAMOWANIA KOMPUTERÓW
+Termin OA (sekcja 2)
+Czas trwania 120 minut.
+Blok 1 [Podstawy programowania - wskaźniki]:
+1) Napisz program, który pobierze od użytkownika dwie liczby, całkowitą wzrost i rzeczywistą waga.
+ Program powinien wymusić na użytkowniku podanie wartości większych od 0 dla obydwóch liczb.
+  Wymuszanie powinno się odbyć oddzielnie dla każdej Z liczb. 
+  Następnie oblicz BMI = wartość waga wzrost² Uzyskany wynik wyświetl z dokładnością do dwóch miejsc po przecinku. BMI wg. wzoru:
+wskaźnika
+2) Następnie program powinien zapytać o liczbę całkowitą a będącą maksymalną wartością jaka może się znaleźć w tablicy dwuwymiarowej T o rozmiarach 5 x 6.
+ Po odczytaniu wartości podanej przez użytkownika wypełnij tablicę losowymi wartościami od 0 do a. Wyświetl sumę wartości dla każdego wiersza tablicy.
+3) Poproś użytkownika o podanie współrzędnych x1, y1, x2, y2.
+ Następnie korzystając ze wskaźników oblicz i wyświetl odległość pomiędzy komórkami pamięci przechowującymi wartość indeksów [x1][y1] i [x2][2] w tablicy T.
